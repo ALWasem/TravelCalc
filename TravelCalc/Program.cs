7eadbb7f7817f006b25d540746be5d9eb2e8f864
Andre Wasem <andre@tim-ash.com>
2019-01-05 21:50:27 -0500

    Created new project for TravelCalc

---
+++ TravelCalc/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TravelCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to TravelCalc.");
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
